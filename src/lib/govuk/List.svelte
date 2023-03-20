8fee4abc9b743dd3e267617398e37bf7ac2316f8
Paulio <pauliorandall@gmail.com>
2023-03-20 15:56:29 +0000

    Add list component

---
+++ src/lib/govuk/List.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let bullets = false
+	export let numbers = false
+	export let spaced = false
+
+	if (bullets && numbers) {
+		throw new Error(
+			'A list cannot have both bullet points and be numbered at the same time'
+		)
+	}
+</script>
+
+{#if numbers}
+	<ol class="govuk-list govuk-list--number" class:govuk-list--spaced="{spaced}">
+		<slot />
+	</ol>
+{:else}
+	<ul
+		class="govuk-list"
+		class:govuk-list--bullet="{bullets}"
+		class:govuk-list--spaced="{spaced}">
+		<slot />
+	</ul>
+{/if}
--- src/routes/abc/+page.svelte
+++ src/routes/abc/+page.svelte
@@ -4,6 +4,7 @@
 	import Col from '$govuk/Col.svelte'
 	import Heading from '$govuk/Heading.svelte'
 	import Link from '$govuk/Link.svelte'
+	import List from '$govuk/List.svelte'
 	import Main from '$govuk/Main.svelte'
 	import Page from '$govuk/Page.svelte'
 	import Paragraph from '$govuk/Paragraph.svelte'
@@ -27,7 +28,7 @@
 		</Heading>
 
 		<Row>
-			<Col one_half two_thirds_desktop>
+			<Col two_thirds one_half_desktop>
 				<Paragraph lead>Leading paragraph.</Paragraph>
 				<Paragraph small>Small paragraph.</Paragraph>
 				<Paragraph centre>Centred aligned paragraph.</Paragraph>
@@ -35,26 +36,28 @@
 				<Paragraph bold>Bold paragraph.</Paragraph>
 				<Paragraph override_font_36>Font size overriden paragraph.</Paragraph>
 
-				<Paragraph>
-					Link <Link href="/abc/xyz">Xyz</Link>.
-				</Paragraph>
-				<Paragraph>
-					Invert link color <span
-						style="background: var(--color-blue); padding: 5px;">
-						<Link invert_color href="/abc/xyz">Xyz.</Link>
-					</span>
-				</Paragraph>
-				<Paragraph>
-					No undlerline link <Link no_underline href="/abc/xyz">Xyz</Link>.
-				</Paragraph>
-				<Paragraph>
-					Link to frequently changing content
-					<Link dynamic_page href="/abc/xyz">Xyz</Link>.
-				</Paragraph>
-				<Paragraph>
-					Link that opens in a new tab
-					<Link as_new_tab href="/abc/xyz">Xyz</Link>.
-				</Paragraph>
+				<List bullets spaced>
+					<li>
+						Link <Link href="/abc/xyz">Xyz</Link>.
+					</li>
+					<li>
+						Invert link color <span
+							style="background: var(--color-blue); padding: 5px;">
+							<Link invert_color href="/abc/xyz">Xyz.</Link>
+						</span>
+					</li>
+					<li>
+						No undlerline link <Link no_underline href="/abc/xyz">Xyz</Link>.
+					</li>
+					<li>
+						Link to frequently changing content
+						<Link dynamic_page href="/abc/xyz">Xyz</Link>.
+					</li>
+					<li>
+						Link that opens in a new tab
+						<Link as_new_tab href="/abc/xyz">Xyz</Link>.
+					</li>
+				</List>
 
 				<PersonCard name="Alice" age="21" />
 				<PersonCard name="Bob" age="69" />
