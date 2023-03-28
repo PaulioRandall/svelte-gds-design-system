2027d475036476763536376ff7da264eac5cf0bf
Paulio <pauliorandall@gmail.com>
2023-03-28 14:41:19 +0100

    Abstract RadioButton from RadioGroup

---
+++ src/lib/govuk/forms/RadioButton.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let id = undefined // ""
+	export let name // = ""
+	export let value // = ""
+	export let label // = ""
+
+	if (!name) {
+		throw new Error('A radio button must have a name')
+	}
+
+	if (!value) {
+		throw new Error('A radio button must have a value')
+	}
+
+	if (!label) {
+		throw new Error('A radio button must have a label')
+	}
+</script>
+
+<div class="govuk-radios__item">
+	<input
+		id="{id}"
+		class="govuk-radios__input"
+		type="radio"
+		name="{name}"
+		value="{value}" />
+	<label class="govuk-label govuk-radios__label" for="{name}">
+		{label}
+	</label>
+</div>
--- src/lib/govuk/forms/RadioGroup.svelte
+++ src/lib/govuk/forms/RadioGroup.svelte
@@ -1,4 +1,6 @@
 <script>
+	import RadioButton from '$govuk/forms/RadioButton.svelte'
+
 	export let id = undefined // ""
 	export let name // = ""
 
@@ -13,22 +15,10 @@
 	if (!options) {
 		throw new Error('A radio group must have some options')
 	}
-
-	// TODO: Might be worth creating a Radio component
 </script>
 
 <div id="{id}" class="govuk-radios" data-module="govuk-radios">
 	{#each options as [value, label, optId]}
-		<div class="govuk-radios__item">
-			<input
-				optId="{optId}"
-				class="govuk-radios__input"
-				type="radio"
-				name="{name}"
-				value="{value}" />
-			<label class="govuk-label govuk-radios__label" for="{name}">
-				{label}
-			</label>
-		</div>
+		<RadioButton id="{optId}" name="{name}" value="{value}" label="{label}" />
 	{/each}
 </div>
