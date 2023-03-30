c375abc25e2e87724d29736e1913ff66700cb987
Paulio <pauliorandall@gmail.com>
2023-03-30 16:07:19 +0100

    Update RadioGroup docs page with new Docs components

--- src/routes/form-components/radio-group/+page.svelte
+++ src/routes/form-components/radio-group/+page.svelte
@@ -1,119 +1,46 @@
 <script>
-	import BreadCrumbs from '$govuk/BreadCrumbs.svelte'
-	import Caption from '$govuk/Caption.svelte'
-	import Heading from '$govuk/Heading.svelte'
 	import Link from '$govuk/Link.svelte'
-	import List from '$govuk/List.svelte'
-	import MenuItem from '$govuk/MenuItem.svelte'
 	import Paragraph from '$govuk/Paragraph.svelte'
-	import SectionBreak from '$govuk/SectionBreak.svelte'
-	import Table from '$govuk/Table.svelte'
-	import WarningText from '$govuk/WarningText.svelte'
 
-	import RadioGroup from '$govuk/forms/RadioGroup.svelte'
+	import DocsPage from '$shared/DocsPage.svelte'
+	import DocsProp from '$shared/DocsProp.svelte'
+	import DocsExample from '$shared/DocsExample.svelte'
 
-	import CodeBlock from '$shared/CodeBlock.svelte'
-	import StandardPage from '$shared/StandardPage.svelte'
-	import Section from '$shared/Section.svelte'
+	import ExampleMinimalist from './ExampleMinimalist.svelte'
+	import exampleMinimalistCode from './ExampleMinimalist.svelte?raw'
 </script>
 
-<StandardPage sticky_menu title="Radio group">
-	<BreadCrumbs
-		slot="breadcrumbs"
-		collapsable
-		crumbs="{[
-			['/home', 'Home'],
-			['/form-components', 'Form components'],
-			['/form-components/radio-group', 'Radio group'],
-		]}" />
-
-	<List slot="side-menu" spaced>
-		<MenuItem bold href="#examples">Examples</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#example-default">Default</MenuItem>
-		</List>
-		<MenuItem bold href="#interface">Interface</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#interface-props">Props</MenuItem>
-		</List>
-	</List>
-
-	<Heading id="page-title" h1 xl>
-		<Caption xl>Form components</Caption>
-		Radio group
-	</Heading>
-
-	<Paragraph>
-		Official documentation on the <Link
+<DocsPage
+	title="Radio group"
+	group="Form components"
+	crumbs="{[
+		['/home', 'Home'],
+		['/form-components', 'Form components'],
+		['/form-components/radio-group', 'Radio group'],
+	]}"
+	examples="{[['example-minimalist', 'Minimalist']]}">
+	<Paragraph lead slot="intro">
+		Official documentation: <Link
 			href="https://design-system.service.gov.uk/components/radios/"
 			>GDS Design System 'Radios' component</Link
 		>.
 	</Paragraph>
 
-	<Section add_top_margin id="examples">
-		<Heading h2 lg>Examples</Heading>
+	<svelte:fragment slot="examples">
+		<DocsExample
+			id="example-minimalist"
+			heading="Minimalist"
+			src="{exampleMinimalistCode}">
+			<ExampleMinimalist />
+		</DocsExample>
+	</svelte:fragment>
 
-		<noscript>
-			<WarningText>
-				Pssst... some examples won't work without JavaScript enabled.
-			</WarningText>
-		</noscript>
-
-		<Heading id="example-default" h3 md>Default</Heading>
-		<RadioGroup
-			id="city-field"
-			name="city"
-			options="{[
-				['london', 'London', 'city-london-id'],
-				['paris', 'Paris', 'city-paris-id'],
-				['madrid', 'Madrid', 'city-madrid-id'],
-				['berlin', 'Berlin', 'city-berlin-id'],
-			]}" />
-		<CodeBlock
-			lines="{[
-				`<script>`,
-				`	import RadioGroup from '$govuk/forms/RadioGroup.svelte'`,
-				`</script>`,
-				``,
-				`<RadioGroup`,
-				`	id="city-field"`,
-				`	name="city"`,
-				`	options="{[`,
-				`		['london', 'London', 'city-london-id'],`,
-				`		['paris',  'Paris',  'city-paris-id'],`,
-				`		['madrid', 'Madrid', 'city-madrid-id'],`,
-				`		['berlin', 'Berlin', 'city-berlin-id'],`,
-				`	]}" />`,
-			]}" />
-	</Section>
-
-	<Section add_top_margin id="interface">
-		<Heading h2 lg>Interface</Heading>
-
-		<Table
-			id="interface-props"
-			caption_md="Props"
-			headers="{[
-				{ key: 'name', label: 'Name' },
-				{ key: 'type', label: 'Type' },
-				{ key: 'summary', label: 'Summary' },
-			]}"
-			records="{[
-				{
-					name: '<code>id<code>',
-					type: 'string',
-					summary: 'Group element ID',
-				},
-				{
-					name: '<code>name<code>',
-					type: 'string',
-					summary: 'Form field name',
-				},
-				{
-					name: '<code>options<code>',
-					type: '[["value", "label", "id"]...]',
-					summary: 'List of options',
-				},
-			]}" />
-	</Section>
-</StandardPage>
+	<svelte:fragment slot="props">
+		<DocsProp name="id" type="string" summary="Wrapper element ID" />
+		<DocsProp name="name" type="string" summary="Form field name" />
+		<DocsProp
+			name="options"
+			type="[['value', 'label', 'id']...]"
+			summary="List of options" />
+	</svelte:fragment>
+</DocsPage>
---
+++ src/routes/form-components/radio-group/ExampleMinimalist.svelte
@@ -0,0 +1,12 @@
+<script>
+	import RadioGroup from '$govuk/forms/RadioGroup.svelte'
+</script>
+
+<RadioGroup
+	name="city"
+	options="{[
+		['london', 'London', 'city-london-id'],
+		['paris', 'Paris', 'city-paris-id'],
+		['madrid', 'Madrid', 'city-madrid-id'],
+		['berlin', 'Berlin', 'city-berlin-id'],
+	]}" />
