1ea124a6d657fa2ed480dd968811370d3ff54e47
Paulio <pauliorandall@gmail.com>
2023-03-30 16:02:53 +0100

    Update ErrorSummary docs page to use new Docs components

--- src/routes/form-components/error-summary/+page.svelte
+++ src/routes/form-components/error-summary/+page.svelte
@@ -1,127 +1,52 @@
 <script>
-	import BreadCrumbs from '$govuk/BreadCrumbs.svelte'
-	import Caption from '$govuk/Caption.svelte'
-	import Heading from '$govuk/Heading.svelte'
-	import InsetText from '$govuk/InsetText.svelte'
 	import Link from '$govuk/Link.svelte'
-	import List from '$govuk/List.svelte'
-	import MenuItem from '$govuk/MenuItem.svelte'
 	import Paragraph from '$govuk/Paragraph.svelte'
-	import Restyle from '$govuk/Restyle.svelte'
-	import SectionBreak from '$govuk/SectionBreak.svelte'
-	import Table from '$govuk/Table.svelte'
 
-	import ErrorSummary from '$govuk/forms/ErrorSummary.svelte'
+	import DocsPage from '$shared/DocsPage.svelte'
+	import DocsProp from '$shared/DocsProp.svelte'
+	import DocsSlot from '$shared/DocsSlot.svelte'
+	import DocsExample from '$shared/DocsExample.svelte'
 
-	import CodeBlock from '$shared/CodeBlock.svelte'
-	import StandardPage from '$shared/StandardPage.svelte'
-	import Section from '$shared/Section.svelte'
+	import ExampleMinimalist from './ExampleMinimalist.svelte'
+	import exampleMinimalistCode from './ExampleMinimalist.svelte?raw'
 </script>
 
-<StandardPage sticky_menu title="Error summary">
-	<BreadCrumbs
-		slot="breadcrumbs"
-		collapsable
-		crumbs="{[
-			['/home', 'Home'],
-			['/form-components', 'Form components'],
-			['/form-components/error-summary', 'Error summary'],
-		]}" />
-
-	<List slot="side-menu" spaced>
-		<MenuItem bold href="#examples">Examples</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#example-list-items">List items</MenuItem>
-		</List>
-		<MenuItem bold href="#interface">Interface</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#interface-props">Props</MenuItem>
-			<MenuItem href="#interface-slots">Slots</MenuItem>
-		</List>
-	</List>
-
-	<Heading id="page-title" h1 xl>
-		<Caption xl>Form components</Caption>
-		Error summary
-	</Heading>
-
-	<Paragraph>
-		Official documentation on the <Link
+<DocsPage
+	title="Date input"
+	group="Form components"
+	crumbs="{[
+		['/home', 'Home'],
+		['/form-components', 'Form components'],
+		['/form-components/error-summary', 'Error summary'],
+	]}"
+	examples="{[['example-minimalist', 'Minimalist']]}">
+	<Paragraph lead slot="intro">
+		Official documentation: <Link
 			href="https://design-system.service.gov.uk/components/error-summary/"
 			>GDS Design System 'Error summary' component</Link
 		>.
 	</Paragraph>
 
-	<Section add_top_margin id="examples">
-		<Heading h2 lg>Examples</Heading>
+	<svelte:fragment slot="examples">
+		<DocsExample
+			id="example-minimalist"
+			heading="Minimalist"
+			src="{exampleMinimalistCode}">
+			<ExampleMinimalist />
+		</DocsExample>
+	</svelte:fragment>
 
-		<Heading id="example-list-items" h3 md>List items</Heading>
-		<ErrorSummary heading="You screwed up somewhere!">
-			<li>
-				<a href="#example-list-items">First error.</a>
-			</li>
-			<li>
-				<a href="#example-list-items">Second error.</a>
-			</li>
-		</ErrorSummary>
-		<CodeBlock
-			lines="{[
-				`<script>`,
-				`	import ErrorSummary from '$govuk/forms/ErrorSummary.svelte'`,
-				`</script>`,
-				``,
-				`<ErrorSummary heading="You screwed up somewhere!">`,
-				`	<li>`,
-				`		<a href="#example-list-items">First error.</a>`,
-				`	</li>`,
-				`	<li>`,
-				`		<a href="#example-list-items">Second error.</a>`,
-				`	</li>`,
-				`</ErrorSummary>`,
-			]}" />
-	</Section>
+	<svelte:fragment slot="props">
+		<DocsProp name="heading" type="string" summary="Heading text" />
+		<DocsProp
+			name="disable_auto_focus"
+			type="string"
+			summary="Disables auto focusing on page load" />
+	</svelte:fragment>
 
-	<Section add_top_margin id="interface">
-		<Heading h2 lg>Interface</Heading>
-
-		<Table
-			id="interface-props"
-			caption_md="Props"
-			headers="{[
-				{ key: 'name', label: 'Name' },
-				{ key: 'type', label: 'Type' },
-				{ key: 'defaultValue', label: 'Default' },
-				{ key: 'summary', label: 'Summary' },
-			]}"
-			records="{[
-				{
-					name: '<code>heading<code>',
-					type: 'string',
-					summary: 'Heading text',
-				},
-				{
-					name: '<code>disable_auto_focus<code>',
-					type: 'bool',
-					defaultValue: 'false',
-					summary: 'Disables auto focusing on page load',
-				},
-			]}" />
-
-		<SectionBreak md />
-
-		<Table
-			id="interface-slots"
-			caption_md="Slots"
-			headers="{[
-				{ key: 'name', label: 'Name' },
-				{ key: 'summary', label: 'Summary' },
-			]}"
-			records="{[
-				{
-					name: '<code>&lt;default&gt;<code>',
-					summary:
-						'List items (&lt;li&gt; elements) usually each linking to the form field in error',
-				},
-			]}" />
-	</Section>
-</StandardPage>
+	<svelte:fragment slot="slots">
+		<DocsSlot
+			default_slot
+			summary="List of &lt;li&gt; elements, usually each links to a form field in error" />
+	</svelte:fragment>
+</DocsPage>
---
+++ src/routes/form-components/error-summary/ExampleMinimalist.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ErrorSummary from '$govuk/forms/ErrorSummary.svelte'
+</script>
+
+<ErrorSummary heading="You screwed up somewhere!">
+	<li>
+		<a href="#example-minimalist">First error.</a>
+	</li>
+	<li>
+		<a href="#example-minimalist">Second error.</a>
+	</li>
+</ErrorSummary>
