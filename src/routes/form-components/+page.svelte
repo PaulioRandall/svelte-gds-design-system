136892e66bb34f2ed377a4bdc7e05b84f3930824
Paulio <pauliorandall@gmail.com>
2023-03-30 16:30:43 +0100

    Reorder form components menu to be alphabetical

--- src/routes/form-components/+page.svelte
+++ src/routes/form-components/+page.svelte
@@ -14,18 +14,18 @@
 		-->
 
 	<List slot="side-menu" spaced>
-		<MenuItem href="/form-components/search-form">Search form</MenuItem>
-		<!--<MenuItem href="/form-components/fieldset">Fieldset</MenuItem>-->
-		<MenuItem href="/form-components/button-group">Button group</MenuItem>
 		<MenuItem href="/form-components/button">Button</MenuItem>
-		<MenuItem href="/form-components/start-button">Start button</MenuItem>
-		<MenuItem href="/form-components/error-summary">Error summary</MenuItem>
-		<MenuItem href="/form-components/text-input">Text input</MenuItem>
-		<MenuItem href="/form-components/select">Select</MenuItem>
-		<MenuItem href="/form-components/radio-group">Radio group</MenuItem>
-		<MenuItem href="/form-components/textarea">Textarea</MenuItem>
+		<MenuItem href="/form-components/button-group">Button group</MenuItem>
 		<MenuItem href="/form-components/checkboxes">Checkboxes</MenuItem>
 		<MenuItem href="/form-components/date-input">Date input</MenuItem>
+		<MenuItem href="/form-components/error-summary">Error summary</MenuItem>
+		<!--<MenuItem href="/form-components/fieldset">Fieldset</MenuItem>-->
 		<!--<MenuItem href="/form-components/file-upload">File upload</MenuItem>-->
+		<MenuItem href="/form-components/radio-group">Radio group</MenuItem>
+		<MenuItem href="/form-components/search-form">Search form</MenuItem>
+		<MenuItem href="/form-components/select">Select</MenuItem>
+		<MenuItem href="/form-components/start-button">Start button</MenuItem>
+		<MenuItem href="/form-components/text-input">Text input</MenuItem>
+		<MenuItem href="/form-components/textarea">Textarea</MenuItem>
 	</List>
 </StandardPage>
