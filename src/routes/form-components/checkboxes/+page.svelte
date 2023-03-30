607145293c76453ff5095125e9ee99883877b299
Paulio <pauliorandall@gmail.com>
2023-03-30 15:50:09 +0100

    Update Checkboxes docs page to use new Docs components

--- src/routes/form-components/checkboxes/+page.svelte
+++ src/routes/form-components/checkboxes/+page.svelte
@@ -1,120 +1,49 @@
 <script>
-	import BreadCrumbs from '$govuk/BreadCrumbs.svelte'
-	import Caption from '$govuk/Caption.svelte'
-	import Heading from '$govuk/Heading.svelte'
 	import Link from '$govuk/Link.svelte'
-	import List from '$govuk/List.svelte'
-	import MenuItem from '$govuk/MenuItem.svelte'
 	import Paragraph from '$govuk/Paragraph.svelte'
 	import SectionBreak from '$govuk/SectionBreak.svelte'
-	import Table from '$govuk/Table.svelte'
-	import WarningText from '$govuk/WarningText.svelte'
 
-	import Checkboxes from '$govuk/forms/Checkboxes.svelte'
+	import DocsPage from '$shared/DocsPage.svelte'
+	import DocsProp from '$shared/DocsProp.svelte'
+	import DocsSlot from '$shared/DocsSlot.svelte'
+	import DocsExample from '$shared/DocsExample.svelte'
 
-	import CodeBlock from '$shared/CodeBlock.svelte'
-	import StandardPage from '$shared/StandardPage.svelte'
-	import Section from '$shared/Section.svelte'
+	import ExampleMinimalist from './ExampleMinimalist.svelte'
+	import exampleMinimalistCode from './ExampleMinimalist.svelte?raw'
 </script>
 
-<StandardPage sticky_menu title="Checkboxes">
-	<BreadCrumbs
-		slot="breadcrumbs"
-		collapsable
-		crumbs="{[
-			['/home', 'Home'],
-			['/form-components', 'Form components'],
-			['/form-components/checkboxes', 'Checkboxes'],
-		]}" />
-
-	<List slot="side-menu" spaced>
-		<MenuItem bold href="#examples">Examples</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#example-minimalist">Minimalist</MenuItem>
-		</List>
-		<MenuItem bold href="#interface">Interface</MenuItem>
-		<List sub_list spaced>
-			<MenuItem href="#interface-props">Props</MenuItem>
-		</List>
-	</List>
-
-	<Heading id="page-title" h1 xl>
-		<Caption xl>Form components</Caption>
-		Checkboxes
-	</Heading>
-
-	<Paragraph>
-		Official documentation on the <Link
+<DocsPage
+	title="Checkboxes"
+	group="Form components"
+	crumbs="{[
+		['/home', 'Home'],
+		['/form-components', 'Form components'],
+		['/form-components/checkboxes', 'Checkboxes'],
+	]}"
+	examples="{[['example-minimalist', 'Minimalist']]}">
+	<Paragraph lead slot="intro">
+		Official documentation: <Link
 			href="https://design-system.service.gov.uk/components/checkboxes/"
-			>GDS Design System 'Checkboxes' component</Link
+			>GDS Design System 'checkboxes' component</Link
 		>.
 	</Paragraph>
 
-	<Section add_top_margin id="examples">
-		<Heading h2 lg>Examples</Heading>
+	<svelte:fragment slot="examples">
+		<DocsExample
+			id="example-minimalist"
+			heading="Minimalist"
+			src="{exampleMinimalistCode}">
+			<ExampleMinimalist />
+		</DocsExample>
+	</svelte:fragment>
 
-		<noscript>
-			<WarningText>
-				Pssst... some examples won't work without JavaScript enabled.
-			</WarningText>
-		</noscript>
-
-		<Heading id="example-minimalist" h3 md>Minimalist</Heading>
-		<Checkboxes
-			name="more-details"
-			options="{[
-				['bacon', 'Bacon'],
-				['lettuce', 'Lettuce'],
-				['tomato', 'Tomato'],
-			]}" />
-		<CodeBlock
-			lines="{[
-				`<script>`,
-				`	import Checkboxes from '$govuk/forms/Checkboxes.svelte'`,
-				`</script>`,
-				``,
-				`<Checkboxes`,
-				`	name="more-details"`,
-				`	options="{[`,
-				`		['bacon', 'Bacon'],`,
-				`		['lettuce', 'Lettuce'],`,
-				`		['tomato', 'Tomato'],`,
-				`	]}" />`,
-			]}" />
-	</Section>
-
-	<Section add_top_margin id="interface">
-		<Heading h2 lg>Interface</Heading>
-
-		<Table
-			id="interface-props"
-			caption_md="Props"
-			headers="{[
-				{ key: 'name', label: 'Name' },
-				{ key: 'type', label: 'Type' },
-				{ key: 'summary', label: 'Summary' },
-			]}"
-			records="{[
-				{
-					name: '<code>id<code>',
-					type: 'string',
-					summary: 'Group element ID',
-				},
-				{
-					name: '<code>name<code>',
-					type: 'string',
-					summary: 'Form field name',
-				},
-				{
-					name: '<code>label<code>',
-					type: 'string',
-					summary: 'Label text',
-				},
-				{
-					name: '<code>options<code>',
-					type: '[["value", "label", "id"]...]',
-					summary: 'List of options',
-				},
-			]}" />
-	</Section>
-</StandardPage>
+	<svelte:fragment slot="props">
+		<DocsProp name="id" type="string" summary="Group ID" />
+		<DocsProp name="name" type="string" summary="Form field name" />
+		<DocsProp name="label" type="string" summary="Label text" />
+		<DocsProp
+			name="options"
+			type="[['value', 'label', 'id']...]"
+			summary="List of options" />
+	</svelte:fragment>
+</DocsPage>
---
+++ src/routes/form-components/checkboxes/ExampleMinimalist.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Checkboxes from '$govuk/forms/Checkboxes.svelte'
+</script>
+
+<Checkboxes
+	name="sandwich-fillings"
+	options="{[
+		['bacon', 'Bacon'],
+		['lettuce', 'Lettuce'],
+		['tomato', 'Tomato'],
+	]}" />
