7ac788c0fc0773e5cac67bedc8c6ec394adb86d6
Paulio <pauliorandall@gmail.com>
2023-04-07 23:30:27 +0100

    Align general & form index pages

--- src/routes/form-components/+page.svelte
+++ src/routes/form-components/+page.svelte
@@ -1,24 +1,42 @@
 <script>
+	import Breadcrumbs from '$govuk/Breadcrumbs.svelte'
+	import Breadcrumb from '$govuk/Breadcrumb.svelte'
+	import Caption from '$govuk/Caption.svelte'
+	import Heading from '$govuk/Heading.svelte'
 	import List from '$govuk/List.svelte'
 	import MenuItem from '$govuk/MenuItem.svelte'
-	//import Paragraph from '$govuk/Paragraph.svelte'
+	import Paragraph from '$govuk/Paragraph.svelte'
 
 	import SideMenuPage from '$shared/SideMenuPage.svelte'
 </script>
 
-<SideMenuPage title="Form components">
-	<!--
-			TODO:
-			+ Pagination
-			+ Cookie banner
-		-->
+<SideMenuPage sticky_menu title="General components">
+	<Breadcrumbs slot="breadcrumbs" collapsable>
+		<Breadcrumb href="/home">Home</Breadcrumb>
+		<Breadcrumb href="/form-components">Form components</Breadcrumb>
+	</Breadcrumbs>
 
-	<List slot="side-menu" spaced>
+	<!--
+		TODO:
+		+ Pagination
+		+ Cookie banner
+	-->
+
+	<Heading id="page-title" h1 xl>
+		<Caption xl>Svelte components</Caption>
+		General components
+	</Heading>
+
+	<List spaced>
 		<MenuItem href="/form-components/button">Button</MenuItem>
-		<MenuItem href="/form-components/button-group">Button group</MenuItem>
+		<List double_indent>
+			<MenuItem href="/form-components/button-group">Button group</MenuItem>
+		</List>
 		<MenuItem href="/form-components/checkboxes">Checkboxes</MenuItem>
 		<MenuItem href="/form-components/date-input">Date input</MenuItem>
-		<MenuItem href="/form-components/date-unit">Date unit</MenuItem>
+		<List double_indent>
+			<MenuItem href="/form-components/date-unit">Date unit</MenuItem>
+		</List>
 		<MenuItem href="/form-components/error-summary">Error summary</MenuItem>
 		<!--<MenuItem href="/form-components/fieldset">Fieldset</MenuItem>-->
 		<!--<MenuItem href="/form-components/file-upload">File upload</MenuItem>-->
